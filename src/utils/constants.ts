59a66fe50f6ddd12ef6da4aa2b99bdd474e5c4cf
minhnguyen2k <minhapplehuna@gmail.com>
2022-12-23 20:04:19 +0700

    update build folder

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-export const APIHost = development ? '/api' : 'http://api.freemics.tech/';
+export const APIHost = development ? '/api' : 'http://api.freemics.tech';
 
 export const ACCESS_TOKEN_KEY = 'comic_system_auth_token';
